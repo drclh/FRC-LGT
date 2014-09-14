77dd5d4502bf8e9c09e195f85333650abddc2d37
drclh <hemmecl@gmail.com>
2014-09-14 12:37:47 -0500

    added POD, comments, variable name changes

--- calc_odds.pl
+++ calc_odds.pl
@@ -7,9 +7,9 @@ use Statistics::ChisqIndep;
 use Text::NSP::Measures::2D::Fisher::right;
 use Getopt::Long;
 
-my $infile;
-my $outfile;
-my $sample_included = 'yes';
+my $infile; # input sample COG abundance file
+my $outfile; # output file
+my $sample_included = 'yes'; # indicates if sample data is included in reference data
 
 GetOptions (
 	"infile=s" => \$infile,
@@ -17,11 +17,11 @@ GetOptions (
 	"sample_included:s" => \$sample_included
 ) or die "Error in command line arguments\n";
 
-my %samp_count;
-my %bact_count;
-my $stats_ref;
-my @orgs;
-my %cog_names;
+my %samp_count; # COG count for samples
+my %ref_count; # COG count for references
+my $stats_ref; # statistics holding variable
+my @orgs; # organisms
+my %cog_names; # COG names
 
 open (my $samp_fh, "<", $infile);
 
@@ -32,41 +32,41 @@ while (<$samp_fh>) { # parse sample abundance data into $samp_count
 	} else {
 		$cog_names{$func_id} = $func_name unless $cog_names{$func_id};
 		foreach my $i (0..$#orgs) {
-			$samp_count{$func_id}{'counts'}{$orgs[$i]} += $counts[$i];
-			$samp_count{$func_id}{'counts'}{'all_ref'} += $counts[$i];
+			$samp_count{$func_id}{'counts'}{$orgs[$i]} += $counts[$i]; # count of individual COG in sample organism
+			$samp_count{$func_id}{'counts'}{'all_ref'} += $counts[$i]; # count of all COGs in sample organisms
 		}
 	}
 }
 close $samp_fh;
 
-opendir(my $dir_dh, ".");
+opendir(my $dir_dh, "."); # get list of reference COG abundance files (currently must be named abund<number>.txt)
 my @files = grep {/^abund\d+\.txt/} readdir($dir_dh);
 close $dir_dh;
 
-foreach (@files) { # parse bacteria abundance data into $bact_count
+foreach (@files) { # parse reference abundance data into $ref_count
 	chomp;
 	open (my $fh, "<", $_);
 	while (<$fh>) {
 		chomp (my ($func_id, $func_name, @counts) = split /\t/);
 		if ($func_id eq 'Func_id') {
-			@orgs = @counts;
+			@orgs = @counts; # get organism names from first line in IMG abundance file
 		} else {
 			$cog_names{$func_id} = $func_name unless $cog_names{$func_id};
 			foreach my $i (0..$#orgs) {
-				$bact_count{$func_id}{'counts'}{$orgs[$i]} += $counts[$i];
-				$bact_count{$func_id}{'counts'}{'all_bact'} += $counts[$i];
+				$ref_count{$func_id}{'counts'}{$orgs[$i]} += $counts[$i]; # count of individual COG in reference organism
+				$ref_count{$func_id}{'counts'}{'all_bact'} += $counts[$i]; # count of all COGs in reference organisms
 			}
 		}
 	}
 	close $fh;
 }
 
-$stats_ref = &calculateOdds({%samp_count}, {%bact_count});
+$stats_ref = &calculateOdds({%samp_count}, {%ref_count}); # calculate odds ratios and chi squared stats
 
 open (my $out_fh, ">", $outfile);
 print $out_fh "Reference Organism\tCOG\tCOG Function\t#COGXXX in reference\t#COGs in reference\t#COGXXXX in bact\t#COGs in bact\tOdds Ratio\tChi Squared Value\tp value\tFisher's Exact Test (1-tailed right)\n";
 
-grep {
+grep { # write out results
 	my $cog = $_;
 	my $name = $cog_names{$cog};
 	grep {
@@ -85,8 +85,8 @@ grep {
 
 my $temp;
 
-sub calculateOdds {
-	my ($references, $all) = @_;
+sub calculateOdds { # calculate odds ratios and chi squared stats
+	my ($samples, $all) = @_;
 	my (%a, %b, %c, $d);
 	my %stats;
   
@@ -94,11 +94,11 @@ sub calculateOdds {
 		my $cog = $_;
 		grep {
 			my $org = $_;
-			my $name = $references->{$cog}{'name'};
-			$a{$cog}{$org} = $references->{$cog}{'counts'}{$org};
-			$b{$org} += $references->{$cog}{'counts'}{$org};
-		} sort keys %{ $references->{$cog}{'counts'} };
-	} sort keys %{ $references };
+			my $name = $samples->{$cog}{'name'};
+			$a{$cog}{$org} = $samples->{$cog}{'counts'}{$org};
+			$b{$org} += $samples->{$cog}{'counts'}{$org};
+		} sort keys %{ $samples->{$cog}{'counts'} };
+	} sort keys %{ $samples };
 
 	grep { # parse all bacteria
 		my $cog = $_;
@@ -109,7 +109,7 @@ sub calculateOdds {
 		} sort keys %{ $all->{$cog}{'counts'} };
 	} sort keys %{ $all };
 
-	grep { # calculate odds ratio, chi squared
+	grep { # calculate odds ratio, chi squared (see POD below for description of odds ratio)
 		my $cog = $_;
 		grep {
 			my $org = $_;
@@ -122,7 +122,8 @@ sub calculateOdds {
 			}
 			
 			$local_c = 1 if $local_c == 0;
-			$stats{$cog}{$org}{'odds ratio'} = sprintf ("%.2f", ($local_a/$local_b) / ($local_c/$d));
+			$stats{$cog}{$org}{'odds ratio'} = sprintf ("%.2f", ($local_a/$local_b) / ($local_c/$d)); # odds ratio
+
 			my $chisq = new Statistics::ChisqIndep;
 			my @obs = ([$local_a, $local_b], [$local_c, $d]);
 			$chisq->load_data(\@obs);
@@ -130,6 +131,7 @@ sub calculateOdds {
 			my $n1p = $local_a + $local_b;
 			my $np1 = $local_c;
 			my $npp = $n1p + $d;
+
 			my $right_value = calculateStatistic( 
 				n11=>$local_a,
 		                n1p=>$n1p,
@@ -154,32 +156,52 @@ sub calculateOdds {
 
 #partial pod of Text::NSP::Measures::2D::Fisher::right that I included for my own reference
 
+#=head1 DESCRIPTION
+
+#Assume that the frequency count data associated with a bigram
+#<word1><word2> is stored in a 2x2 contingency table:
+
+#          word2   ~word2
+#  word1    n11      n12 | n1p
+# ~word1    n21      n22 | n2p
+#           --------------
+#           np1      np2   npp
+
+#where n11 is the number of times <word1><word2> occur together, and
+#n12 is the number of times <word1> occurs with some word other than
+#word2, and n1p is the number of times in total that word1 occurs as
+#the first word in a bigram.
+
+#The odds ratio computes the ratio of the number of times that
+#the words in a bigram occur together (or not at all) to the
+#number of times the words occur individually. It is the cross
+#product of the diagonal and the off-diagonal.
+
+#Thus, ODDS RATIO = n11*n22/n21*n12
+
+#if n21 and/or n12 is 0, then each zero value is "smoothed" to one to
+#avoid a zero in the denominator.
+
+#=over
+
+#=cut
+
+# POD documentation - main docs before the code
+
+=head1 NAME
+
+calc_odds.pl - calculate odds ratios and chi squared statistics for COG abundance data from IMG
+
 =head1 DESCRIPTION
 
-Assume that the frequency count data associated with a bigram
-<word1><word2> is stored in a 2x2 contingency table:
+This program take sample and reference COG abundance profile tables from IMG and calculates odds ratios and chi squared statistics for each COG in each sample.  The references (e.g. "all bacteria") are read from the current directory as a list of files (currently abund<num>.txt) due to the fact that IMG only allows >1000 genomes to be calculated at once.  These files are concatenated into a single variable.  Sample input file is individual COG abundance profiles.  The sample_included parameter indicates if the sample data is included in the reference data.  If so, the sample data is subtracted out to give the proper odds ratio.  Common examples of setting sample_included would be comparing a metagenome or an isolate genome not in IMG's database to all sequenced bacteria.  sample_included defaults to "yes".
 
-          word2   ~word2
-  word1    n11      n12 | n1p
- ~word1    n21      n22 | n2p
-           --------------
-           np1      np2   npp
+=head1 DEPENDENCIES
 
-where n11 is the number of times <word1><word2> occur together, and
-n12 is the number of times <word1> occurs with some word other than
-word2, and n1p is the number of times in total that word1 occurs as
-the first word in a bigram.
+Statistics::ChisqIndep
+Text::NSP::Measures::2D::Fisher::right
+Getopt::Long
 
-The odds ratio computes the ratio of the number of times that
-the words in a bigram occur together (or not at all) to the
-number of times the words occur individually. It is the cross
-product of the diagonal and the off-diagonal.
-
-Thus, ODDS RATIO = n11*n22/n21*n12
-
-if n21 and/or n12 is 0, then each zero value is "smoothed" to one to
-avoid a zero in the denominator.
-
-=over
+=head1 AUTHORS - Chris Hemme
 
 =cut
